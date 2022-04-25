3951caebf0ff57fe52eec02675573439d3dfa1f3
Andrey Klimentyev <andrey.klimentyev@flant.com>
2022-04-25 17:46:02 +0300

    Improvements (#48)
    A round of improvements

---
+++ pkg/registry_checker/checker_test.go
@@ -0,0 +1,28 @@
+package registry_checker
+
+import (
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_parseImageName(t *testing.T) {
+	const (
+		goodImageName                = "docker.io/test:test"
+		goodImageNameWithoutRegistry = "test:test"
+		badImageName                 = "te*^#@@st"
+
+		defaultRegistryName = "test-registry.io"
+	)
+
+	_, err := parseImageName(goodImageName, "")
+	require.NoError(t, err)
+
+	_, err = parseImageName(badImageName, "")
+	require.Error(t, err)
+
+	ref, err := parseImageName(goodImageNameWithoutRegistry, defaultRegistryName)
+	require.NoError(t, err)
+	require.Equal(t, ref.Name(), path.Join(defaultRegistryName, goodImageNameWithoutRegistry))
+}
