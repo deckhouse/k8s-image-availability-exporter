fb1c87e7ea1ad155f691bb35277e7d4afd07bc40
alexey.komyakov <alexey.komyakov@flant.com>
2025-01-21 11:04:27 +0300

    Add handling of incorrect dokcer hub URLs
    Signed-off-by: alexey.komyakov <alexey.komyakov@flant.com>

--- pkg/providers/k8s/k8s.go
+++ pkg/providers/k8s/k8s.go
@@ -2,9 +2,11 @@ package k8s
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	kubeauth "github.com/google/go-containerregistry/pkg/authn/kubernetes"
 	corev1 "k8s.io/api/core/v1"
+	"strings"
 
 	"github.com/google/go-containerregistry/pkg/authn"
 )
@@ -21,9 +23,44 @@ func NewProvider(pullSecretsGetter func(image string) []corev1.Secret) *Provider
 	}
 }
 
+func (p Provider) correctDockerRegistry(secrets []corev1.Secret) ([]corev1.Secret, error) {
+	for i, secret := range secrets {
+		if secret.Type == corev1.SecretTypeDockerConfigJson {
+			data, exists := secret.Data[corev1.DockerConfigJsonKey]
+			if exists {
+				var dockerConfig map[string]interface{}
+				if err := json.Unmarshal(data, &dockerConfig); err == nil {
+					auths, ok := dockerConfig["auths"].(map[string]interface{})
+					if ok {
+						for url := range auths {
+							if strings.Contains(url, "docker.io") && url != "https://index.docker.io/v1/" {
+								auths["https://index.docker.io/v1/"] = auths[url]
+								delete(auths, url)
+							}
+						}
+						updatedData, err := json.Marshal(dockerConfig)
+						if err == nil {
+							secrets[i].Data[corev1.DockerConfigJsonKey] = updatedData
+						} else {
+							return nil, fmt.Errorf("failed to re-marshal docker config: %v", err)
+						}
+					}
+				} else {
+					return nil, fmt.Errorf("failed to unmarshal docker config: %v", err)
+				}
+			}
+		}
+	}
+	return secrets, nil
+}
+
 func (p Provider) GetAuthKeychain(registry string) (authn.Keychain, error) {
 	dereferencedPullSecrets := p.pullSecretsGetter(registry)
-	kc, err := kubeauth.NewFromPullSecrets(context.TODO(), dereferencedPullSecrets)
+	correctedSecrets, err := p.correctDockerRegistry(dereferencedPullSecrets)
+	if err != nil {
+		return nil, err
+	}
+	kc, err := kubeauth.NewFromPullSecrets(context.TODO(), correctedSecrets)
 	if err != nil {
 		return nil, fmt.Errorf("error while processing keychain from secrets: %w", err)
 	}
